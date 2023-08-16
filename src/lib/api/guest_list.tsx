2db79ad05a730137eca431b080fb28ef3942ece3
Jake Yin <munfei.yin@volservers.com>
2023-08-16 15:43:10 +0800

    env URL Variable

--- src/lib/api/guest_list.tsx
+++ src/lib/api/guest_list.tsx
@@ -1,13 +1,16 @@
 'use client'
 
+const baseurl = process.env.NEXT_PUBLIC_API_URL
+
 export async function searchGuest(keyword : string)
 {
-    const url = `http://127.0.0.1:4201/guests-by-details/?keyword=${keyword}`;
+    console.log(baseurl);
+    const url = `${baseurl}guests-by-details/?keyword=${keyword}`;
     const res = await fetch(url)
     return res.json();
 }
 
 export async function update()
 {
-    
+
 }
