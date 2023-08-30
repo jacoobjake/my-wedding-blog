700d3ca7b594369021689522d0cc3f3c42c7b4c5
Jake Yin <munfei.yin@volservers.com>
2023-08-30 09:53:38 +0800

    Add openGraph image

--- src/app/(default)/layout.tsx
+++ src/app/(default)/layout.tsx
@@ -1,10 +1,17 @@
-'use client'
+// 'use client'
 
-import { OpenModalContextProvider, CurrentUserContextProvider, ToastProvider } from '../context-provider'
+import { ToastProvider } from '../context-provider'
 import Header from '@/components/layout/header'
 import DefaultFooter from '@/components/layout/footer'
-import { useRouter, usePathname } from 'next/navigation'
 import RsvpModal from '@/components/modals/rsvp'
+import type { Metadata } from 'next'
+export const metadata: Metadata = {
+    openGraph: {
+        title: "Jake & Yee Huan",
+        images: "https://onedrive.live.com/embed?resid=332220CB5ABF1FD3%21388&authkey=%21AFJMjukNGlyuTws&width=1080&height=1080",
+        alternateLocale: "Jake & Yee Huan",
+    }
+}
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
@@ -15,22 +22,16 @@ export default function Layout({
 }: {
     children: React.ReactNode
 }) {
-    const router = useRouter();
-    const pathname = usePathname();
     return (
         <html className="h-full bg-white">
             <body className="h-full">
                 <div className="min-h-full">
-                        {/* <OpenModalContextProvider>
-                            <CurrentUserContextProvider> */}
-                                <ToastProvider>
-                                    <Header />
-                                    <main className="pt-14">
-                                        {children}
-                                    </main>
-                                </ToastProvider>
-                            {/* </CurrentUserContextProvider>
-                        </OpenModalContextProvider> */}
+                    <ToastProvider>
+                        <Header />
+                        <main className="pt-14">
+                            {children}
+                        </main>
+                    </ToastProvider>
                     <RsvpModal></RsvpModal>
                     <DefaultFooter></DefaultFooter>
                 </div>
